206446ad7bc7dec9633b354affbdf5f30e139fc9
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-19 16:57:46 -0500

    made components and put them in a folder then imported them in the app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
+import Explanation from "./Components/Explanation"
+import Image from "./Components/Image"
+import Title from "./Components/Title"
 
 function App() {
   return (
     <div className="App">
+      {/* <Title/>
+      <Image/>
+      <Explanation/> */}
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
