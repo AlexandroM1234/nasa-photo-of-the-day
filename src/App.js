f8ed295de826586aa1c758cb2720356b82468fb7
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-19 17:35:19 -0500

    used props and imported all the components for the content on the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Explanation from "./Components/Explanation"
 import Image from "./Components/Image"
 import Title from "./Components/Title"
+import Axios from "axios";
 
 function App() {
+  const [nasaData,setNasaData]=useState({})
+  useEffect(()=>{
+    Axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then(response=>{
+          setNasaData(response.data);
+        })
+        .catch(error=>{console.log("something went wrong", error)}) 
+  },[]);
   return (
     <div className="App">
-      {/* <Title/>
-      <Image/>
-      <Explanation/> */}
+      <Title title={nasaData.title}/>
+      <Image hdurl={nasaData.hdurl}/>
+      <Explanation explanation={nasaData.explanation}/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
---
+++ src/Components/Image.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Image = ({hdurl})=>{
+
+    return <img src={hdurl}/>
+
+}
+
+export default Image
